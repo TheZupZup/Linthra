88eddf7dbc2738acc5780a2a5d941b0fb3f8dece
TheZupZup <16434778+TheZupZup@users.noreply.github.com>
2026-07-17 16:40:08 -0400

    style: format GitHub sponsor token provider

--- lib/data/repositories/github_sponsor_token_store_provider.dart
+++ lib/data/repositories/github_sponsor_token_store_provider.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/repositories/github_sponsor_token_store.dart';
 import 'secure_github_sponsor_token_store.dart';
 
-final githubSponsorTokenStoreProvider = Provider<GitHubSponsorTokenStore>((ref) {
+final githubSponsorTokenStoreProvider =
+    Provider<GitHubSponsorTokenStore>((ref) {
   return const SecureGitHubSponsorTokenStore();
 });
