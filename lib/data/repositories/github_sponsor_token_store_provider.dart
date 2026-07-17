bbb87b2ec02099c8395de4affda6618909be1c48
TheZupZup <16434778+TheZupZup@users.noreply.github.com>
2026-07-17 16:26:40 -0400

    feat: provide GitHub sponsor token storage

---
+++ lib/data/repositories/github_sponsor_token_store_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../core/repositories/github_sponsor_token_store.dart';
+import 'secure_github_sponsor_token_store.dart';
+
+final githubSponsorTokenStoreProvider = Provider<GitHubSponsorTokenStore>((ref) {
+  return const SecureGitHubSponsorTokenStore();
+});
